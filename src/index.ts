a8c9c3efe6e31b6fbaf018c9507443bbee2f11e3
Ihor <igornepipenko@gmail.com>
2020-01-25 14:46:55 +0200

    Feature: special types [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,15 @@
-class UIElement {
-    animate(_options: IAnimateOptions,): void {
-
-    }
-}
-
-interface IAnimateOptions {
-    delay: number;
-    easing: 'easy-in' | 'easy-out'
-}
-
-
-new UIElement().animate({delay: 1000, easing: 'easy-in'});
-
-interface IFact {
-    factId: number;
-    users: string[];
-    factValue: string
-}
-
-enum TooltipDirection {
-    TOP,
-    LEFT,
-    BOTTOM,
-    RIGHT
-}
-
-const uniqueValue = (): keyof typeof TooltipDirection=> {  // 'factId' |  'users' | 'factValue'
-    return 'RIGHT'
+let anyType: any = {};
+anyType.a = 1;
+anyType['b'] = () => {
 };
+anyType = 1;
+anyType();
 
-const dataList: { action: string, data: IFact }[] = [];
-dataList.map((item: { action: string, data: IFact }) => {
-    if (item.data[uniqueValue()] === 2) {
-        return item;
-    }
-    return item;
-})
+let unkType: unknown = {};
+unkType.a = 1;
+unkType['b'] = () => {
+};
+unkType = 1;
+unkType();
+
+let v: void = undefined;
