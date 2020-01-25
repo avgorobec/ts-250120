6f1cc78a97cb20276c735545bd2456980813bae5
Ihor <igornepipenko@gmail.com>
2020-01-25 14:38:30 +0200

    Feature: literal types [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,38 @@
-// interface UserAccount {
-//     firstName: string;
-//     age: number;
-// }
+class UIElement {
+    animate(_options: IAnimateOptions,): void {
 
-// let p1 = UserAccount
-//
-// let userAccount = {
-//     firstName: 'Ihor',
-//     age: 33
-// }
-//
-// let u1: Response = 1;
-//
-// class UserAccount {
-//     firstName!: string;
-//     age!: number;
-// }
-// //
-// let u2 = new UserAccount();
-
-
-let isDone: boolean = true;
-
-let nan: number = NaN;
-let decimal: number = 6;
-let binar: number = 0b0101;
-
-let color: string = 'blue';
-
-let nill: null = null;
-let und: undefined = undefined;
-
-let bigInt: bigint = 4n;
-
-
-const key1 = Symbol('key1');
-let key2: symbol = Symbol('key2');
-let key3: symbol = Symbol('key3');
-
-let strictObj = {
-    [key1]: 'value 1',
-    [key2]: 'value 2',
+    }
 }
 
-let val1 = strictObj[key2];
+interface IAnimateOptions {
+    delay: number;
+    easing: 'easy-in' | 'easy-out'
+}
+
+
+new UIElement().animate({delay: 1000, easing: 'easy-in'});
+
+interface IFact {
+    factId: number;
+    users: string[];
+    factValue: string
+}
+
+enum TooltipDirection {
+    TOP,
+    LEFT,
+    BOTTOM,
+    RIGHT
+}
+
+const uniqueValue = (): keyof typeof TooltipDirection=> {  // 'factId' |  'users' | 'factValue'
+    return 'RIGHT'
+};
+
+const dataList: { action: string, data: IFact }[] = [];
+dataList.map((item: { action: string, data: IFact }) => {
+    if (item.data[uniqueValue()] === 2) {
+        return item;
+    }
+    return item;
+})
